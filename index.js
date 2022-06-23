62dbe675d6c853a1f9c442948de1d7c184c42d47
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-23 14:37:08 +0200

    exo 2 terminé

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const cat = {
     age: "3",
     isCute: true,
 }
-if (cat.isCute === true){
-    console.log("so cute");
-} else {
-    console.log("not so cute");
+// if (cat.isCute === true){
+//     console.log("so cute");
+// } else {
+//     console.log("not so cute");
+// }
+
+// 02 - Combine
+
+const cat2 = {
+    name: "Tom",
+    age: "30",
+    isCute: false,
 }
+
+const cats = [cat, cat2]
+// console.log(cats);
+// console.log(cats[0].age);
+console.log(cats[1].isCute);
