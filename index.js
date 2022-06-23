97ad983ad37a9115e9a4d5285d653852bee5125d
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-23 14:29:22 +0200

    exo 1 terminé

---
+++ index.js
@@ -0,0 +1,12 @@
+// 01 - Object
+
+const cat = {
+    name: "Garfield",
+    age: "3",
+    isCute: true,
+}
+if (cat.isCute === true){
+    console.log("so cute");
+} else {
+    console.log("not so cute");
+}
